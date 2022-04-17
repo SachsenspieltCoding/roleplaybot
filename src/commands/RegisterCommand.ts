f77054e0a60a24f015659e367b6f224513867680
SACHSΞΠSPIΞLT <mail@sachsenspielt.de>
2022-04-17 13:23:20 +0200

    added options to RegisterCommand.ts
    Signed-off-by: SACHSΞΠSPIΞLT <mail@sachsenspielt.de>

--- src/commands/RegisterCommand.ts
+++ src/commands/RegisterCommand.ts
@@ -1,16 +1,43 @@
-import {BaseCommandInteraction, Client} from 'discord.js'
-import Command from 'src/Command'
+import { BaseCommandInteraction, Client, Permissions } from "discord.js";
+import Command from "src/Command";
+import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
 
 const Register: Command = {
-  name: 'register',
-  description: 'Registriert einen User für das Roleplay Projekt.',
+  name: "register",
+  description: "Registriert einen User für das Roleplay Projekt.",
+  options: [
+    {
+      name: "vorname",
+      description: "Der Vorname deines Roleplay-Charakters",
+      required: true,
+      type: ApplicationCommandOptionTypes.STRING,
+    },
+    {
+      name: "nachname",
+      description: "Der Nachname deines Roleplay-Charakters",
+      required: true,
+      type: ApplicationCommandOptionTypes.STRING,
+    },
+    {
+      name: "beruf",
+      description: "Der Beruf deines Roleplay-Charakters",
+      required: true,
+      type: ApplicationCommandOptionTypes.STRING,
+    },
+  ],
   execute: async (client: Client, interaction: BaseCommandInteraction) => {
-    await interaction.reply(
-        `Hallo Welt! Mein Name ist ${client.user!.tag}, deiner ist ${
-            interaction.user.tag
-        }!`
-    )
-  },
-}
+    if (!interaction.memberPermissions?.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      await interaction.reply(
+        `Dieser Befehl ist noch nicht vollständig implementiert!`
+      );
+    }
 
-export default Register
+    await interaction.reply(
+      `Vorname: ${interaction.options.get("vorname")?.value}\nNachname: ${
+        interaction.options.get("nachname")?.value
+      }\nBeruf: ${interaction.options.get("beruf")?.value}`
+    );
+  },
+};
+
+export default Register;
